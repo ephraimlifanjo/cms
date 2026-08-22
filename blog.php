06fc12fb0839df28b3f2cdbe631b35330acd146c
ephraim lifanjo <ephraimlifanjos@gmail.com>
2026-08-22 14:56:48 +0100

    refactor(routes): consolidate public blog on index route

--- blog.php
+++ blog.php
@@ -1,11 +1,1 @@
-<?php include 'db.php';
-$result = $conn->query("SELECT * FROM articles ORDER BY created_at DESC");
-while($row = $result->fetch_assoc()) {
-    echo '<h2>' . htmlspecialchars($row['title']) . '</h2>';
-    echo '<p>' . nl2br(htmlspecialchars($row['content'])) . '</p>';
-    if ($row['image']) {
-        echo '<img src="uploads/' . htmlspecialchars($row['image']) . '" style="max-width:200px;">';
-    }
-    echo '<hr>';
-}
-?>
+<?php header('Location: index.php', true, 301); exit;
